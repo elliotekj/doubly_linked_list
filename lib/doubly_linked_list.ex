701e8f2b35b18b2fa36ca408b9d9bac249b22a4e
Elliot Jackson <elliotjackson@mailbox.org>
2023-11-02 16:54:26 +0000

    add head and tail insertion

--- lib/doubly_linked_list.ex
+++ lib/doubly_linked_list.ex
@@ -1,18 +1,46 @@
 defmodule DoublyLinkedList do
-  @moduledoc """
-  Documentation for `DoublyLinkedList`.
-  """
+  defstruct head: nil, tail: nil, nodes: %{}
 
-  @doc """
-  Hello world.
+  alias DoublyLinkedList.Node
 
-  ## Examples
+  def new, do: %__MODULE__{}
 
-      iex> DoublyLinkedList.hello()
-      :world
+  def insert_head(%__MODULE__{} = dll, data), do: update_head(dll, data)
+  def insert_head({%__MODULE__{} = dll, _node}, data), do: update_head(dll, data)
 
-  """
-  def hello do
-    :world
+  def insert_tail(%__MODULE__{} = dll, data), do: update_tail(dll, data)
+  def insert_tail({%__MODULE__{} = dll, _node}, data), do: update_tail(dll, data)
+
+  defp update_head(%__MODULE__{head: head, tail: tail, nodes: nodes} = dll, data) do
+    node = Node.new(data, next: head)
+    nodes = nodes |> Map.put(node.__id__, node) |> update_head_pointer(head, node.__id__)
+
+    {%{dll | nodes: nodes, head: node.__id__, tail: tail || node.__id__}, node}
+  end
+
+  defp update_tail(%__MODULE__{head: head, tail: tail, nodes: nodes} = dll, data) do
+    node = Node.new(data, prev: tail)
+    nodes = nodes |> Map.put(node.__id__, node) |> update_tails(tail, node.__id__)
+
+    {%{dll | nodes: nodes, head: head || node.__id__, tail: node.__id__}, node}
+  end
+
+  defp update_head_pointer(nodes, nil, _new_head), do: nodes
+
+  defp update_head_pointer(nodes, current_head, new_head) do
+    Map.update!(nodes, current_head, fn node -> %{node | prev: new_head} end)
+  end
+
+  defp update_tails(nodes, nil, _new_tail), do: nodes
+
+  defp update_tails(nodes, current_tail, new_tail) do
+    Map.update!(nodes, current_tail, fn node -> %{node | next: new_tail} end)
   end
 end
+
+# TODO
+# - insert_before
+# - insert_after
+# - insert_beginning
+# - insert_end
+# - remove
--- test/doubly_linked_list_test.exs
+++ test/doubly_linked_list_test.exs
@@ -1,8 +1,69 @@
 defmodule DoublyLinkedListTest do
   use ExUnit.Case
+  alias DoublyLinkedList, as: DLL
   doctest DoublyLinkedList
 
-  test "greets the world" do
-    assert DoublyLinkedList.hello() == :world
+  describe "new/0" do
+    test "returns a new DLL" do
+      assert DLL.new() == %DLL{}
+    end
+  end
+
+  describe "insert_head/2" do
+    test "inserts into an empty list" do
+      {dll, _} = %DLL{} |> DLL.insert_head("test")
+
+      assert dll.head == dll.tail
+      assert map_size(dll.nodes) == 1
+    end
+
+    test "updates a singularly populated list" do
+      {dll, _} = %DLL{} |> DLL.insert_head("test")
+      old_head = dll.head
+      {dll, _} = DLL.insert_head(dll, "test2")
+
+      refute dll.head == old_head
+      assert dll.tail == old_head
+      assert map_size(dll.nodes) == 2
+    end
+
+    test "updates a populated list" do
+      {dll, _} = %DLL{} |> DLL.insert_head("test") |> DLL.insert_head("test2")
+      old_head = dll.head
+      {dll, _} = DLL.insert_head(dll, "test3")
+
+      refute dll.head == old_head
+      refute dll.tail == old_head
+      assert map_size(dll.nodes) == 3
+    end
+  end
+
+  describe "insert_tail/2" do
+    test "inserts into an empty list" do
+      {dll, _} = %DLL{} |> DLL.insert_tail("test")
+
+      assert dll.head == dll.tail
+      assert map_size(dll.nodes) == 1
+    end
+
+    test "updates a singularly populated list" do
+      {dll, _} = %DLL{} |> DLL.insert_tail("test")
+      old_tail = dll.tail
+      {dll, _} = DLL.insert_tail(dll, "test2")
+
+      refute dll.tail == old_tail
+      assert dll.head == old_tail
+      assert map_size(dll.nodes) == 2
+    end
+
+    test "updates a populated list" do
+      {dll, _} = %DLL{} |> DLL.insert_tail("test") |> DLL.insert_tail("test2")
+      old_tail = dll.tail
+      {dll, _} = DLL.insert_tail(dll, "test3")
+
+      refute dll.tail == old_tail
+      refute dll.head == old_tail
+      assert map_size(dll.nodes) == 3
+    end
   end
 end
